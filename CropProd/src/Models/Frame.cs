df24039084042f4e0c14ddde150e00c6409490c2
awwar <saysdragon007@gmail.com>
2019-02-21 01:51:50 +0300

    Add multithreading tile loader

--- CropProd/src/Models/Frame.cs
+++ CropProd/src/Models/Frame.cs
@@ -7,17 +7,27 @@ namespace Models
     class Frame
     {
         public Image image = null; //изображение 
+        public string path;
+        public string url;
         public Vector2 lefttop = new Vector2(0, 0); //левый-верхний угол
         public Vector2 scenecoord = new Vector2(0, 0);//координаты на сцене
         public Vector2 coord = new Vector2(0, 0);//координаты на сцене
 
-        public Frame(Image image, Vector2 coord)
+        public Frame(Vector2 coord,Image image)
         {
             this.image = image;
             this.coord = coord;
             this.lefttop.X = coord.X * 256;
             this.lefttop.Y = coord.Y * 256;
-            Console.WriteLine(lefttop.ToString());
+        }
+
+        public Frame(string path, string url, Vector2 coord)
+        {
+            this.url = url;
+            this.path = path;
+            this.coord = coord;
+            this.lefttop.X = coord.X * 256;
+            this.lefttop.Y = coord.Y * 256;
         }
 
     }
